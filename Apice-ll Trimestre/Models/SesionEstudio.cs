eb46dd8c15215314d86b1786a7ae9f4bf884a2e1
YeinerVindas <120690628@covao.ed.cr>
2026-06-14 11:30:19 -0600

    sesion estudio

--- Apice-ll Trimestre/Models/SesionEstudio.cs
+++ Apice-ll Trimestre/Models/SesionEstudio.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Apice_ll_Trimestre.Models
 {
@@ -19,15 +20,18 @@ namespace Apice_ll_Trimestre.Models
         public DateTime Fecha { get; set; }
 
         [Required]
-        [Display(Name = "Duración total (minutos)")]
-        public int Duracion { get; set; }
+        [Range(1, 16, ErrorMessage = "El número de bloques debe estar entre 1 y 16.")]
+        [Display(Name = "Cantidad de bloques (Pomodoros)")]
+        public int CantidadBloques { get; set; }
+
+        [Display(Name = "Bloque actual")]
+        public int BloqueActual { get; set; } = 1;
+
+        [Display(Name = "En descanso")]
+        public bool EnDescanso { get; set; } = false;
 
         [Display(Name = "Tiempo transcurrido (minutos)")]
         public int TiempoTranscurrido { get; set; } = 0;
-
-        [Required]
-        [Display(Name = "Cantidad de bloques")]
-        public int CantidadBloques { get; set; }
 
         [Display(Name = "Completada")]
         public bool Completada { get; set; } = false;
@@ -36,10 +40,23 @@ namespace Apice_ll_Trimestre.Models
         [Display(Name = "Estado")]
         public string Estado { get; set; } // "Pendiente", "En progreso", "Pausada", "Completada"
 
-        [Display(Name = "Tiempo restante (minutos)")]
-        public int TiempoRestante => Duracion - TiempoTranscurrido;
-
         [Display(Name = "Trasladada a siguiente sesión")]
         public bool TrasladadaASiguiente { get; set; } = false;
+
+        // Calculadas
+        [NotMapped]
+        public int DuracionTotal => CantidadBloques * 25; // minutos de estudio puro
+
+        [NotMapped]
+        public int DescansoTotal => (CantidadBloques - 1) * 5; // descansos entre bloques
+
+        [NotMapped]
+        public int TiempoTotalSesion => DuracionTotal + DescansoTotal;
+
+        [NotMapped]
+        public int TiempoRestante => TiempoTotalSesion - TiempoTranscurrido;
+
+        [NotMapped]
+        public int BloquesCompletados => TiempoTranscurrido / 30; // 25 estudio + 5 descanso
     }
 }
